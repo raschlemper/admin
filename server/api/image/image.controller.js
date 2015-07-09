e61b5c29fe524d11ad4684bd2fa2d707e27d9e00
rafael.schlemper <rafael.schlemper@gennera.com.br>
2015-07-09 15:35:52 -0300

    ajuste tamanho image

--- server/api/image/image.controller.js
+++ server/api/image/image.controller.js
@@ -32,12 +32,63 @@ var createImageUser = function(file, name) {
     var path = 'public/image/users/' + name;
     lwip.open(file.path, function(err, image) {
         if (err) throw err;
-        image.resize(180, 250, function(err, rzdImg) {
-            rzdImg.writeFile(path, function(err) {
+        var size = calculateImageSize(image);
+        image.resize(size.width, size.height, function(err, rzdImg) {
+            rzdImg.writeFile(path, function(err, image) {
                 if (err) throw err;
+                return image;
             });
         });
     });
+}
+
+var calculateImageSize = function(image) {
+    var widthMax = 200;
+    var heightMax = 240;
+    var width = image.width();
+    var height = image.height();
+    return getSize(widthMax, heightMax, width, height);
+}
+
+var getSize = function(widthMax, heightMax, width, height) {
+    var newWidth = getNewSize(heightMax, height, width);
+    var newHeight = getNewSize(widthMax, width, height);
+    var size = {
+        'width': widthMax,
+        'height': heightMax
+    }
+    size = verifyHeightSize(heightMax, newWidth, newHeight);
+    size = verifyWidthSize(widthMax, newWidth, newHeight);
+    return size;
+}
+
+var verifyWidthSize = function(widthMax, newWidth, newHeight) {
+    if(widthMax > newWidth) return;
+    return {
+        'width': widthMax;
+        'height': newHeight;      
+    }
+}
+
+var verifyHeightSize = function(heightMax, newWidth, newHeight) {
+    if(heightMax > newHeight) return;
+    return {
+        'width': newWidth;
+        'height': heightMax;
+    }   
+}
+
+var getNewSize = function(sizeMax, size, comparate) {
+    var perc = percentual(sizeMax, size);
+    return calculateSize(comparate, perc);    
+}
+
+var percentual = function(sizeMax, size) {
+    return (sizeMax * 100) / size;
+}
+
+var calculateSize = function(size, percentual) {
+    return (size * percentual) / 100;
 }
 
 var fileName = function(type, name) {
@@ -67,5 +118,3 @@ var decode = function(value) {
 exports.create = create;
 exports.createImageUser = createImageUser;
 exports.fileName = fileName;
-exports.encode = encode;
-exports.decode = decode;
