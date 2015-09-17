f309355dd3821c82802938659fa86f62157a7d6d
rafael.schlemper <rafael.schlemper@gennera.com.br>
2015-09-17 09:15:18 -0300

    ajustes botoes

--- public/app/directive/submenu.directive.js
+++ public/app/directive/submenu.directive.js
@@ -11,11 +11,14 @@ app.directive( 'submenu', function ( ) {
         link: function(scope, element, attrs) {
             scope.menus = [];
             var menuUser = { 'label': 'MENU.USERS', 'state': 'users' };
+            var menuUserView = { 'label': 'MENU.USER.VIEW', 'state': 'userView' };
             if(scope.currentMenu === 'MENU.USERS') {
                 scope.menus = [];
             } else if(scope.currentMenu === 'MENU.USER.CREATE') {
                 scope.menus = [ menuUser ];
             } else if(scope.currentMenu === 'MENU.USER.VIEW') {
+                scope.menus = [ menuUser ];
+            } else if(scope.currentMenu === 'MENU.USER.SYSTEM') {
                 scope.menus = [ menuUser ];
             }
         }
